81d95312dec11e6ad42bf60554117e7d1a48d825
Olivier Maisonneuve <olivierm@ex-situ.com>
2017-09-20 10:39:45 -0400

    Update tests.

--- Tests/Service/TrackingParameterManager/ChaturbateTrackingParameterFormatterTest.php
+++ Tests/Service/TrackingParameterManager/ChaturbateTrackingParameterFormatterTest.php
@@ -4,88 +4,18 @@ namespace EXS\LanderTrackingChaturbateBundle\Tests\Service\TrackingParameterMana
 
 use EXS\LanderTrackingChaturbateBundle\Service\TrackingParameterManager\ChaturbateTrackingParameterManager;
 use Symfony\Component\HttpFoundation\ParameterBag;
-use Symfony\Component\HttpFoundation\Request;
 
 class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testExtractWithoutParametersNorCookies()
+    public function testExtractFromQuery()
     {
-        $request = $this->prophesize(Request::class);
-
-        $query = $this->prophesize(ParameterBag::class);
-        $query->get('track')->willReturn(null)->shouldBeCalledTimes(1);
-
-        $request->query = $query;
-
-        $cookies = $this->prophesize(ParameterBag::class);
-        $cookies->get('cmp', 1)->willReturn(1)->shouldBeCalledTimes(1);
-        $cookies->get('exid')->willReturn(null)->shouldBeCalledTimes(1);
-        $cookies->get('visit')->willReturn(null)->shouldBeCalledTimes(1);
-
-        $request->cookies = $cookies;
-
-        $manager = new ChaturbateTrackingParameterManager(1);
-
-        $result = $manager->extract($request->reveal());
-
-        $this->assertCount(3, $result);
-
-        $this->assertArrayHasKey('cmp', $result);
-        $this->assertEquals(1, $result['cmp']);
-
-        $this->assertArrayHasKey('exid', $result);
-        $this->assertNull($result['exid']);
-
-        $this->assertArrayHasKey('visit', $result);
-        $this->assertNull($result['visit']);
-    }
-
-    public function testExtractWithoutParametersButCookies()
-    {
-        $request = $this->prophesize(Request::class);
-
-        $query = $this->prophesize(ParameterBag::class);
-        $query->get('track')->willReturn(null)->shouldBeCalledTimes(1);
-
-        $request->query = $query;
-
-        $cookies = $this->prophesize(ParameterBag::class);
-        $cookies->get('cmp', 1)->willReturn(123)->shouldBeCalledTimes(1);
-        $cookies->get('exid')->willReturn('UUID987654321')->shouldBeCalledTimes(1);
-        $cookies->get('visit')->willReturn('5')->shouldBeCalledTimes(1);
-
-        $request->cookies = $cookies;
-
-        $manager = new ChaturbateTrackingParameterManager(1);
-
-        $result = $manager->extract($request->reveal());
-
-        $this->assertCount(3, $result);
-
-        $this->assertArrayHasKey('cmp', $result);
-        $this->assertEquals(123, $result['cmp']);
-
-        $this->assertArrayHasKey('exid', $result);
-        $this->assertEquals('UUID987654321', $result['exid']);
-
-        $this->assertArrayHasKey('visit', $result);
-        $this->assertEquals('5', $result['visit']);
-    }
-
-    public function testExtractWithParameters()
-    {
-        $request = $this->prophesize(Request::class);
-
         $query = $this->prophesize(ParameterBag::class);
         $query->get('track')->willReturn('123~UUID987654321~5', '123~UUID987654321', '123')->shouldBeCalledTimes(3);
-
-        $request->query = $query;
-
-        $revealedRequest = $request->reveal();
+        $revealedQuery = $query->reveal();
 
         $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $manager->extract($revealedRequest);
+        $result = $manager->extractFromQuery($revealedQuery);
 
         $this->assertCount(3, $result);
 
@@ -98,9 +28,11 @@ class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('visit', $result);
         $this->assertEquals(5, $result['visit']);
 
-        $result = $manager->extract($revealedRequest);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $this->assertCount(3, $result);
+        $result = $manager->extractFromQuery($revealedQuery);
+
+        $this->assertCount(2, $result);
 
         $this->assertArrayHasKey('cmp', $result);
         $this->assertEquals(123, $result['cmp']);
@@ -108,35 +40,27 @@ class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('exid', $result);
         $this->assertEquals('UUID987654321', $result['exid']);
 
-        $this->assertArrayHasKey('visit', $result);
-        $this->assertEquals(1, $result['visit']);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $manager->extract($revealedRequest);
+        $result = $manager->extractFromQuery($revealedQuery);
 
-        $this->assertCount(3, $result);
+        $this->assertCount(1, $result);
 
         $this->assertArrayHasKey('cmp', $result);
         $this->assertEquals(123, $result['cmp']);
-
-        $this->assertArrayHasKey('exid', $result);
-        $this->assertNull($result['exid']);
-
-        $this->assertArrayHasKey('visit', $result);
-        $this->assertEquals(1, $result['visit']);
     }
-
 
     public function testFormatWithEmptyArray()
     {
         $trackingParameters = new ParameterBag([]);
 
-        $formatter = new ChaturbateTrackingParameterManager(1);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $formatter->format($trackingParameters);
+        $result = $manager->format($trackingParameters);
 
         $this->assertCount(1, $result);
         $this->assertArrayHasKey('track', $result);
-        $this->assertEquals('1', $result['track']);
+        $this->assertNull($result['track']);
     }
 
     public function testFormatWithAllParameters()
@@ -147,9 +71,9 @@ class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
             'visit' => 5,
         ]);
 
-        $formatter = new ChaturbateTrackingParameterManager(1);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $formatter->format($trackingParameters);
+        $result = $manager->format($trackingParameters);
 
         $this->assertCount(1, $result);
         $this->assertArrayHasKey('track', $result);
@@ -163,13 +87,13 @@ class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
             'exid' => 'UUID987654321',
         ]);
 
-        $formatter = new ChaturbateTrackingParameterManager(1);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $formatter->format($trackingParameters);
+        $result = $manager->format($trackingParameters);
 
         $this->assertCount(1, $result);
         $this->assertArrayHasKey('track', $result);
-        $this->assertEquals('123~UUID987654321~1', $result['track']);
+        $this->assertEquals('123~UUID987654321', $result['track']);
     }
 
     public function testFormatWithCmp()
@@ -178,12 +102,23 @@ class ChaturbateTrackingParameterManagerTest extends \PHPUnit_Framework_TestCase
             'cmp' => 123,
         ]);
 
-        $formatter = new ChaturbateTrackingParameterManager(1);
+        $manager = new ChaturbateTrackingParameterManager(1);
 
-        $result = $formatter->format($trackingParameters);
+        $result = $manager->format($trackingParameters);
 
         $this->assertCount(1, $result);
         $this->assertArrayHasKey('track', $result);
         $this->assertEquals('123', $result['track']);
+    }
+
+    public function testInitialize()
+    {
+        $manager = new ChaturbateTrackingParameterManager(1);
+
+        $result = $manager->initialize();
+
+        $this->assertCount(1, $result);
+        $this->assertArrayHasKey('cmp', $result);
+        $this->assertEquals(1, $result['cmp']);
     }
 }
